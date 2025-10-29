3996f92ecd6217ff3a70aa00bd139233168de4f8
Wojtek <wlachow1@charlotte.edu>
2025-10-28 22:08:38 -0400

    Made sure that home page simply does not show error to avoid confusion

--- src/server.js
+++ src/server.js
@@ -20,6 +20,10 @@ app.get('/health', (req, res) => {
 
 app.use('/tasks', taskRoutes);
 
+app.get('/', (req, res) => {
+  res.status(200).json({ status: 'Home page, nothing else should appear here' });
+});
+
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Not found' });
 });
